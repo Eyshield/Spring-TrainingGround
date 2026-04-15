59d105534f1994f3fa63b6cae2eda536720f05e8
Eyshield <samy.amaouche008@gmail.com>
2026-04-15 13:35:38 +0200

    feature: adding relation between User with project and task

--- trainingApi/trainingApi/src/main/java/com/trainingApi/User/User.java
+++ trainingApi/trainingApi/src/main/java/com/trainingApi/User/User.java
@@ -1,12 +1,15 @@
 package com.trainingApi.User;
 
 import com.trainingApi.Common.Role;
+import com.trainingApi.Project.Project;
+import com.trainingApi.Task.Task;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -28,7 +31,10 @@ public class User implements UserDetails {
     private String username;
     @Enumerated(EnumType.STRING)
     private Role role;
-
+    @ManyToMany
+    private Collection<Project>projects=new ArrayList<>();
+    @OneToMany(mappedBy = "user")
+    private Collection<Task>tasks=new ArrayList<>();
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
