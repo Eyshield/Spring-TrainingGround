db9df15c1130897bb9e5318aad41616d624a092b
Eyshield <samy.amaouche008@gmail.com>
2026-04-15 13:31:16 +0200

    feature: adding relation between column and prokect and tasks   Correction:Column is reserved name in postgres

--- trainingApi/trainingApi/src/main/java/com/trainingApi/Column/Column.java
+++ trainingApi/trainingApi/src/main/java/com/trainingApi/Column/Column.java
@@ -1,24 +1,31 @@
 package com.trainingApi.Column;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.trainingApi.Project.Project;
+import com.trainingApi.Task.Task;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Table(name = "columns_table")
 public class Column {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String title;
     private String position;
+    @ManyToOne
+    private Project project;
+    @OneToMany(mappedBy = "column")
+    private Collection<Task>tasks=new ArrayList<>();
 
 }
+
