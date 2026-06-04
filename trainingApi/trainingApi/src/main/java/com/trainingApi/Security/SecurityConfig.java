e235cb95b5c26c7654551916b744486fd7a932c2
Eyshield <samy.amaouche008@gmail.com>
2026-06-04 17:12:08 +0200

    added the endpoints for testing

--- trainingApi/trainingApi/src/main/java/com/trainingApi/Security/SecurityConfig.java
+++ trainingApi/trainingApi/src/main/java/com/trainingApi/Security/SecurityConfig.java
@@ -36,8 +36,16 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/user/**").permitAll()
+                        .requestMatchers("/api/task/**").permitAll()
+                        .requestMatchers("/api/project/**").permitAll()
+                        .requestMatchers("/api/column/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
